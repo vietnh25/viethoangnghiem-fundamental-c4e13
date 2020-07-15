b6c120c0da1193b103629595a8271ea772bab3d6
howtolove488 <viethoang.nghiem@gmail.com>
2020-07-15 14:27:37 +0700

    inital commit

---
+++ session2/demo.py
@@ -0,0 +1,9 @@
+# number = int(input("how many lapp?: "))
+# print(sum(range(number)))
+num1 = int(input("how many lapp?: "))
+list = []
+for i in range(num1):
+    list.append(i)
+
+listSum = sum(list)
+print(listSum)    
