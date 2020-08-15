9608ad4ae1b9c103c16476461be2551c2f7f57c4
howtolove48 <48313429+howtolove48@users.noreply.github.com>
2020-08-15 10:34:46 +0700

    Update cakes_itl.py

--- Fundamental/mysql_db/cakes_itl.py
+++ Fundamental/mysql_db/cakes_itl.py
@@ -2,7 +2,7 @@ import pymysql
 from pymongo import MongoClient
 
 mongo_client = MongoClient()
-mongo_database = mongo_client.get_database('cakes_ex')
+mongo_database = mongo_client.get_database('cakes')
 mongo_collection = mongo_database.get_collection('cakes')
 
 client = pymysql.connect(
@@ -14,192 +14,142 @@ client = pymysql.connect(
 
 database = client.cursor()
 
-database.execute("CREATE DATABASE cakes")
+# database.execute("CREATE DATABASE cakes")
 
-database.execute('''
-    CREATE TABLE cakes.cakes(
-        _id VARCHAR(255),
-        type VARCHAR(255),
-        name VARCHAR(255),
-        ppu DECIMAL(10,2),
-        PRIMARY KEY(_id)
-    )
-''')
+# database.execute('''
+#     CREATE TABLE cakes.cakes(
+#         _id VARCHAR(255),
+#         type VARCHAR(255),
+#         name VARCHAR(255),
+#         ppu DECIMAL(10,2),
+#         PRIMARY KEY(_id)
+#     )
+# ''')
 
-database.execute('''
-    CREATE TABLE cakes.batters(
-        id INT,
-        type VARCHAR(255),
-        PRIMARY KEY(id)
-    )
-''')
+# database.execute('''
+#     CREATE TABLE cakes.batters(
+#         id INT,
+#         type VARCHAR(255),
+#         PRIMARY KEY(id)
+#     )
+# ''')
 
-database.execute('''
-    CREATE TABLE `cakes`.topping(
-        id INT,
-        type VARCHAR(255),
-        PRIMARY KEY (id)
-    )
-''')
+# database.execute('''
+#     CREATE TABLE `cakes`.topping(
+#         id INT,
+#         type VARCHAR(255),
+#         PRIMARY KEY (id)
+#     )
+# ''')
 
-database.execute('''
-    CREATE TABLE `cakes`.filling(
-        id INT,
-        type VARCHAR(255),
-        PRIMARY KEY (id)
-    )
-''')
+# database.execute('''
+#     CREATE TABLE `cakes`.filling(
+#         id INT,
+#         type VARCHAR(255),
+#         PRIMARY KEY (id)
+#     )
+# ''')
 
-database.execute('''
-    CREATE TABLE `cakes`.cake_batter(
-        cake_id VARCHAR(255),
-        batter_id INT,
-        PRIMARY KEY (cake_id, batter_id)
-    )
-''')
+# database.execute('''
+#     CREATE TABLE `cakes`.cake_batter(
+#         cake_id VARCHAR(255),
+#         batter_id INT,
+#         PRIMARY KEY (cake_id, batter_id)
+#     )
+# ''')
 
-database.execute('''
-    CREATE TABLE `cakes`.cake_topping(
-        cake_id VARCHAR(255),
-        topping_id INT,
-        PRIMARY KEY (cake_id, topping_id)
-    )
-''')
+# database.execute('''
+#     CREATE TABLE `cakes`.cake_topping(
+#         cake_id VARCHAR(255),
+#         topping_id INT,
+#         PRIMARY KEY (cake_id, topping_id)
+#     )
+# ''')
 
-database.execute('''
-    CREATE TABLE `cakes`.cake_filling(
-        cake_id VARCHAR(255),
-        filling_id INT,
-        PRIMARY KEY (cake_id, filling_id)
-    )
-''')
+# database.execute('''
+#     CREATE TABLE cakes.fillings(
+#         id INT,
+#         name VARCHAR(255),
+#         addcost DECIMAL(10,2),
+#         PRIMARY KEY (id, addcost)
+#     )
+# ''')
 
 # Cakes table
 query = {
-    '_id':{'$ne':None},
-    'type':{'$ne':None},
-    'name':{'$ne':None},
-    'ppu':{'$ne':None},
-    'batters':{'$ne':None},
-    'filling':{'$ne':None},
-    'topping':{'$ne':None}
+    'type': {'$ne': None},
+    'name': {'$ne': None},
+    'ppu': {'$ne': None}
 }
-
-cakes_database = mongo_collection.find(query)
-
-for cakes in cakes_database:
-    cakes_id = str(cakes['_id'])
+cakes = mongo_collection.find(query)
+for cake in cakes:
     database.execute(f'''
-        INSERT INTO `cakes`.cakes(`id`,`type`,`name`,`ppu`)
-        VALUES("{cakes_id}","{cakes['type']}","{cakes['name']}","{cakes['ppu']}")
+        INSERT INTO `cakes`.cakes(`_id`, `type`, `name`, `ppu`)
+        VALUES("{cake['_id']}", "{cake['type']}", "{cake['name']}", {cake['ppu']})
     ''')
+
 
 # Batters table
 query = [
-  {
-    '$unwind':'$batters'
-  },
-  {
-    '$group':{
-      '_id':'$batters'
-    }
-  }
+    {'$unwind': '$batters.batter'},
+    {'$group': {'_id': '$batters.batter'}}
 ]
-
-batters_database = mongo_collection.aggregate(query)
-
-for batters in batters_database:
+batters = mongo_collection.aggregate(query)
+for batter in batters:
     database.execute(f'''
-        INSERT INTO `cakes`.batters(`name`)
-        VALUES("{batters['_id']}")
+        INSERT INTO `cakes`.batters(`id`, `type`)
+        VALUES({batter['_id']['id']}, "{batter['_id']['type']}")
     ''')
 
 # Cakes_batters table
-query = [
-  {
-    '$unwind':'$batters'
-  }
-]
-
-cakes_batters_database = mongo_collection.aggregate(query)
-
-for cakes_batters in cakes_batters_database:
-    cakes_batters_id = str(cakes_batters["_id"])
+query = [{'$unwind': '$batters.batter'}]
+cake_batter_database = mongo_collection.aggregate(query)
+for cake_batter in cake_batter_database:
     database.execute(f'''
-        INSERT INTO `cakes`.cakes_batters(`cakes_id`,`batters_name`)
-        VALUES("{cakes_batters_id}","{cakes_batters['batters']}")
+        INSERT INTO `cakes`.cake_batter(`cake_id`, `batter_id`)
+        VALUES("{cake_batter['_id']}", {cake_batter['batters']['batter']['id']})
     ''')
 
 # Topping table
 query = [
-  {
-    '$unwind':'$topping'
-  },
-  {
-    '$group':{
-      '_id':'$topping'
-    }
-  }
+    {'$unwind': '$topping'},
+    {'$group': {'_id': '$topping'}}
 ]
-
-topping_database = mongo_collection.aggregate(query)
-
-for topping in topping_database:
+toppings = mongo_collection.aggregate(query)
+for topping in toppings:
     database.execute(f'''
-        INSERT INTO `cakes`.topping(`name`)
-        VALUES("{topping['_id']}")
+        INSERT INTO `cakes`.topping(`id`, `type`)
+        VALUES({topping['_id']['id']}, "{topping['_id']['type']}")
     ''')
 
 # Cakes_topping table
-query = [
-  {
-    '$unwind':'$topping'
-  }
-]
-
-cakes_topping_database = mongo_collection.aggregate(query)
-
-for cakes_topping in cakes_topping_database:
-    cakes_topping_id = str(cakes_topping["_id"])
+query = [{'$unwind': '$topping'}]
+cake_topping_database = mongo_collection.aggregate(query)
+for cake_topping in cake_topping_database:
     database.execute(f'''
-        INSERT INTO `cakes`.cakes_topping(`cakes_id`,`topping_name`)
-        VALUES("{cakes_topping_id}","{cakes_topping['topping']}")
+        INSERT INTO `cakes`.cake_topping(`cake_id`, `topping_id`)
+        VALUES("{cake_topping['_id']}", {cake_topping['topping']['id']})
     ''')
 
 # Fillings table
 query = [
-  {
-    '$unwind':'$fillings'
-  },
-  {
-    '$group':{
-      '_id':'$fillings'
-    }
-  }
+    {'$unwind': '$fillings.filling'},
+    {'$group': {'_id': '$fillings.filling'}}
 ]
-
-fillings_database = mongo_collection.aggregate(query)
-
-for fillings in fillings_database:
+fillings = mongo_collection.aggregate(query)
+for filling in fillings:
     database.execute(f'''
-        INSERT INTO `cakes`.fillings(`name`)
-        VALUES("{fillings['_id']}")
+        INSERT INTO `cakes`.fillings(`id`, `name`, `addcost`)
+        VALUES({filling['_id']['id']}, "{filling['_id']['name']}", {filling['_id']['addcost']})
     ''')
 
 # Cakes_fillings table
-query = [
-  {
-    '$unwind':'$fillings'
-  }
-]
-
-cakes_fillings_database = mongo_collection.aggregate(query)
-
-for cakes_fillings in cakes_fillings_database:
-    cakes_fillings_id = str(cakes_fillings["_id"])
+query = [{'$unwind': '$fillings.filling'}]   
+cake_filling_database = mongo_collection.aggregate(query)
+for cake_filling in cake_filling_database:
     database.execute(f'''
-        INSERT INTO `cakes`.cakes_fillings(`cakes_id`,`fliings_name`)
-        VALUES("{cakes_fillings_id}","{cakes_fillings['fillings']}")
-    ''')
+        INSERT INTO `cakes`.cake_filling(`cake_id`, `filling_id`)
+        VALUES("{cake_filling['_id']}", {cake_filling['fillings']['filling']['id']})
+    ''') 
 
 client.commit()
